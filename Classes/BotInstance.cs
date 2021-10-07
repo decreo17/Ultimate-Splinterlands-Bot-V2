3223a1e4d4354d5f30c7d5be149e8d7f7cad948f
decreo17 <decreogithub@gmail.com>
2021-10-07 20:26:13 +0800

    final update on xpath as added by jones

--- Classes/BotInstance.cs
+++ Classes/BotInstance.cs
@@ -715,7 +715,7 @@ namespace Ultimate_Splinterlands_Bot_V2.Classes
         {
             try
             {
-                var rating = driver.FindElement(By.XPath("//div[@class='progress__info']/span[@class='number_text']")).GetAttribute("innerHTML");
+                var rating = driver.FindElement(By.XPath("//div[@class='league_status_panel_progress_bar_pos']//span[@class='number_text']")).GetAttribute("innerHTML");
                 return rating; //jones
             }
             catch (Exception ex)
